ea9c11ff486942e2b56ac899050e1ba9c4fb92ce
gretaivan <greta.ivan91@gmail.com>
2021-03-09 17:46:38 +0000

    reverted back to merged lsitener and handler

--- bundle.js
+++ bundle.js
@@ -33,7 +33,7 @@ function searchButton() {
     console.log(query)
     document.getElementById('search-button').addEventListener("click", async (e) => {
         e.preventDefault()
-        const data = await apiFuncs.getData(`http://localhost:3000/search?${query}`)
+        const data = await apiFuncs.getData(`http://localhost:3000/search/:${query}`)
         // handle responses
         console.log(data)
     })
@@ -43,7 +43,7 @@ function luckyButton() {
     const query = document.getElementById('search-bar').value
     document.getElementById('lucky-button').addEventListener("click", async (e) => {
         e.preventDefault()
-        const data = await apiFuncs.getData(`http://localhost:3000/search/lucky?${query}`)
+        const data = await apiFuncs.getData(`http://localhost:3000/search/lucky/:${query}`)
         // redirect to random page 
         // window.location.href = 'dataUrl...'
     })
