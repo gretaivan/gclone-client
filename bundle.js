bc01fafdbc52630ba355d2dd0b465657af4129b1
chris westerduin <chriswesterduin@outlook.com>
2021-03-09 16:52:30 +0000

    api error

---
+++ bundle.js
@@ -0,0 +1,57 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+// Example Get method implementation:
+async function getData(url = '') {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+
+  module.exports = {
+      getData
+  }
+},{}],2:[function(require,module,exports){
+const listeners = require('./listeners')
+
+listeners.searchButton()
+listeners.luckyButton()
+},{"./listeners":3}],3:[function(require,module,exports){
+const apiFuncs = require('./api')
+
+function searchButton() {
+    const query = document.getElementById('search-bar').value
+    console.log(query)
+    document.getElementById('search-button').addEventListener("click", async (e) => {
+        e.preventDefault()
+        const data = await apiFuncs.getData(`http://localhost:3000/search?${query}`)
+        // handle responses
+        console.log(data)
+    })
+}
+
+function luckyButton() {
+    const query = document.getElementById('search-bar').value
+    document.getElementById('lucky-button').addEventListener("click", async (e) => {
+        e.preventDefault()
+        const data = await apiFuncs.getData(`http://localhost:3000/search/lucky?${query}`)
+        // redirect to random page 
+        // window.location.href = 'dataUrl...'
+    })
+}
+
+
+module.exports = {
+    searchButton,
+    luckyButton
+}
+},{"./api":1}]},{},[2]);
